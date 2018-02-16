ea8d9b1eb32dc57b062ac4d53fb9ef47c345d851
mariobro <mario.martinez271@gmail.com>
2018-02-15 19:46:25 -0600

    modified file by adapting it to cacheVectors exmpl

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+##cool so basically it starts with making an empty matrix
+## in it we have to have a setter and getter for the matrix
+## and a setter and getter for the inverse woo
 
-## Write a short comment describing this function
+## we don't actually implement the inverse function until cacheSolve tho
+
+## we can pretty much make something analogous to 
+## the vector version
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv ##DO I NEED THE SETINV EVEN? 
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
@@ -12,4 +28,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached matrix inverse")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)##WoO U DIDIT DO U NEED THE ...??
+  x$setinv(m)
+  m
+  
 }
